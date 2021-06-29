1933334a888c49b6ef395ad78fb923edc7d0650b
anubhuti25 <anubhuti.prabha@gmail.com>
2021-06-29 14:22:18 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var app = express();
+
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get('/json',(req,res) => {
-    res.json({"message": "Hello json"})
+    console.log(process.env)
+    if(process.env.MESSAGE_STYLE == 'uppercase')
+        res.json({"message": "Hello json".toUpperCase()})
+    else
+        res.json({"message": "Hello json"})
 })
 app.get('/',(req,res) => {
     res.sendFile(__dirname + '/views/index.html')
