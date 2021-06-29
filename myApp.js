7601acd56eebbeb1319601e86a33c16692a9d0b7
anubhuti25 <anubhuti.prabha@gmail.com>
2021-06-29 22:17:53 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,13 +1,36 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 
+app.use((req,res,next) => {
+    console.log(req.method + ' ' + req.path + ' - ' + req.ip)
+    next();
+})
+
 app.use("/public", express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.get('/now',
+    (req,res,next) => {
+        req.time = new Date().toString()
+        next()
+    },
+    (req, res) => {
+        res.send({time: req.time})
+    }
+)
+
+app.get('/:word/echo', (req,res,next) => {
+    let word = req.params.word;
+    res.send({echo: word})
+})
+
+app.get('/name', (req,res,next) => {
+    res.send({ name: req.query.first + ' ' + req.query.last })
+})
 
 app.get('/json',(req,res) => {
-    console.log(process.env)
-    if(process.env.MESSAGE_STYLE == 'uppercase')
-        res.json({"message": "Hello json".toUpperCase()})
-    else
         res.json({"message": "Hello json"})
 })
 app.get('/',(req,res) => {
