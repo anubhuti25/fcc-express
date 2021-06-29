cf8dbc17981ef5aa2bd1d9d32c8a17d56b133016
anubhuti25 <anubhuti.prabha@gmail.com>
2021-06-29 13:17:41 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,42 +1,6 @@
 var express = require('express');
 var app = express();
 app.get('/',(req,res) => {
-    res.send('Hello Express');
+    res.sendFile(__dirname + '/views/index.html')
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
